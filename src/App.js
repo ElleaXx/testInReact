2996cccaf3f77f866b012b8639387b902542ff56
ElleaXx <54738990+ElleaXx@users.noreply.github.com>
2020-03-20 17:06:49 +0300

    добавил запросы

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect  } from 'react';
+import React, { Component} from 'react';
 import './App.css';
 import Nav from './components/Nav'
 import Main from './components/Main'
@@ -13,75 +13,26 @@ class App extends Component {
     
     edit: '',
 
-    time: '', 
-    createdDate: '', 
-    createdTime: '', 
-    status: '', 
-    quantityPerson: '', 
-    quantityTime: '', 
-    name: '', 
-    phone: '', 
-    hall: '',
+    waiting: '',
+    status: '',
+    count: '',
+    time: '',
+    name: '',
+    phone: '',
+    halls: '',
+    created: '',
 
     modalIsOpen:false,
-    cards: [
-      {
-        time: '20.45', 
-        createdDate: 'Создан 03.02.2019', 
-        createdTime: '14:16', 
-        status: 'Готов', 
-        quantityPerson: '10', 
-        quantityTime: '6', 
-        name: 'Денис', 
-        phone: '8-999-555-44-22', 
-        hall: '15, Основной зал'
-      },
-      {
-       time: '08.25', 
-        createdDate: 'Создан 13.01.2020',
-        createdTime: '24:06', 
-        status: 'Ожидание', 
-        quantityPerson: '5', 
-        quantityTime: '4', 
-        name: 'Алексей', 
-        phone: '8-883-157-56-88', 
-        hall: '10, Малый зал'
-      },
-      {
-        time: '14.02', 
-        createdDate: 'Создан 22.01.2018', 
-        createdTime: '12:16', 
-        status: 'Отмена', 
-        quantityPerson: '22', 
-        quantityTime: '16', 
-        name: 'Олег', 
-        phone: '8-569-124-04-20', 
-        hall: '1, Основной зал'},
-    ],
+
+    data: [],
   }
   // добавление компонента
-  funcObgect = () => {
-    let cards = this.state.cards
-    cards.push(
-      {
-        time: '20.45', 
-        createdDate: 'Создан 03.02.2019', 
-        createdTime: '14:16', 
-        status: 'Готов', 
-        quantityPerson: '10', 
-        quantityTime: '6', 
-        name: 'Денис', 
-        phone: '8-999-555-44-22', 
-        hall: '1, Основной зал'
-      }
-    )
-    this.setState({cards: cards})
-  }
+
 // удаление компонента
     funcObgectDelete = (index) => {
-      let cards = this.state.cards;
-      cards.splice(index,1);
-      this.setState({cards: cards})
+      let {data} = this.state
+      data.splice(index,1);
+      this.setState({data: data})
     }
     openModal = () => {
       this.setState({modalIsOpen: true})
@@ -93,8 +44,17 @@ class App extends Component {
 
 
 
+
+    handleChangeWaiting = (event) => {
+      this.setState({waiting: event.target.value});
+    }
+    handleChangeStatus = (event) => {
+      this.setState({status: event.target.value});
+    }
+    handleChangeCount = (event) => {
+      this.setState({count: event.target.value});
+    }
     handleChangeTime = (event) => {
-      console.log(this.state.time)
       this.setState({time: event.target.value});
     }
     handleChangeName = (event) => {
@@ -103,132 +63,174 @@ class App extends Component {
     handleChangePhone = (event) => {
       this.setState({phone: event.target.value});
     }
-    handleChangeStatus = (event) => {
-      this.setState({status: event.target.value});
+    handleChangeHalls = (event) => {
+      this.setState({halls: event.target.value});
     }
-    handleChangeQuantityPerson = (event) => {
-      this.setState({quantityPerson: event.target.value});
+    handleChangeCreated = (event) => {
+      this.setState({created: event.target.value})
     }
-    handleChangeQuantityTime = (event) => {
-      this.setState({quantityTime: event.target.value});
-    }
-    handleChangeHall = (event) => {
-      this.setState({hall: event.target.value});
-    }
-
-    // handleSubmitTime = () => {
-    //   let a = this.state.time
-    //   alert('Время ' + a)
-    // }
-    // handleSubmitName = () => {
-    //   let a = this.state.value
-    //   alert('Ваше имя ' + a)
-    // }
-    // handleSubmitPhone = () => {
-    //   let a = this.state.value
-    //   alert('Ваш телефон ' + a)
-    // }
 
     handleSubmit = () => {
-      let { time, name, phone, status, quantityPerson, quantityTime, hall, cards, edit} = this.state;
-      if(edit == '' ) {
-        cards.push({
-            time: time, 
-            createdDate: name, 
-            createdTime: '14:16', 
-            status: status, 
-            quantityPerson: quantityPerson, 
-            quantityTime: quantityTime, 
-            name: name, 
-            phone: phone, 
-            hall: hall
-          }
-        )
+      let {id, data ,edit, waiting, status, count, time, name, phone, halls, created} = this.state;
+      if(edit === '' ) {
+        let form = new FormData()
+        form.append( 'waiting', waiting);
+        form.append( 'status', status);
+        form.append( 'count', count);
+        form.append( 'time', time);
+        form.append( 'name', name);
+        form.append( 'phone', phone);
+        form.append( 'halls', halls);
+        form.append( 'created', created);
+        fetch('http://testmax.serverway.ru/api/user/add', 
+        {
+          method: 'post', 
+          body: form
+        }
+        ).then((response) => {
+          return response.json();
+        }).then((data) => {
+          console.log(data)
+          this.getData()
+        });
       }
       else {
-        cards[edit].time = time;
-        cards[edit].createdTime = '14.16';
-        cards[edit].status = status;
-        cards[edit].quantityPerson = quantityPerson;
-        cards[edit].quantityTime = quantityTime;
-        cards[edit].name = name;
-        cards[edit].phone = phone;
-        cards[edit].hall = hall;
+        data[edit].id = id;
+        let form = new FormData()
+        form.append( 'waiting', waiting);
+        form.append( 'status', status);
+        form.append( 'count', count);
+        form.append( 'time', time);
+        form.append( 'name', name);
+        form.append( 'phone', phone);
+        form.append( 'halls', halls);
+        form.append( 'created', created);
+        fetch('http://testmax.serverway.ru/api/user/update', 
+        {
+          method: 'post', 
+          body: form
+        }
+        ).then((response) => {
+          return response.json();
+        }).then((data) => {
+          console.log(data)
+          this.getData()
+        });
 
       }
-      this.setState({modalIsOpen: false,
-        edit: '',
-        time: '', 
-        createdDate: '', 
-        createdTime: '', 
-        status: '', 
-        quantityPerson: '', 
-        quantityTime: '', 
-        name: '', 
-        phone: '', 
-        hall: ''})
+      this.setState(
+        {modalIsOpen: false,
+          waiting: '',
+          status: '',
+          count: '',
+          time: '',
+          name: '',
+          phone: '',
+          halls: '',
+          created: '',
+        }
+      )
     }
 
     edit = (elem,index) => {
       this.setState({
         edit: index,
         modalIsOpen: true,
-        time: elem.time, 
-        createdDate: elem.name, 
-        createdTime: '14:16', 
-        status: elem.status, 
-        quantityPerson: elem.quantityPerson, 
-        quantityTime: elem.quantityTime, 
-        name: elem.name, 
-        phone: elem.phone, 
-        hall: elem.hall
-      })
+        waiting: elem.waiting,
+        status: elem.status,
+        count: elem.count,
+        time: elem.time,
+        name: elem.name,
+        phone: elem.phone,
+        halls: elem.halls,
+        created: elem.created
+        }
+      )
     }
 
+// создается и работает после render
+componentWillMount() {
+  console.log('willMount-Complete - создается и работает после render ')
+}
 
-    Example = () => {
-      const [count, setCount] = useState(0);
-    
-      useEffect(() => {
-        document.title = `Вы нажали ${count} раз`;
-      })
-    }
+componentDidMount() {
+  console.log('ditMount-Complete - создается и работает после render ')
+  this.getData()
+}
+
+getData = () => {
+  fetch('http://testmax.serverway.ru/api/user/records').then((response) => {
+    console.log('response', response)
+    return response.json();
+  }).then((data) => {
+    console.log(data)
+    this.setState ({
+      data: data.data
+    })
+  });
+}
+
+componentDidUpdate() {
+  console.log('componentDidUpdate - обновление компонента')
+}
+
+componentWillUnmount() {
+  console.log('componentWillUnmount - создается и работает после render и удаления компонента')
+}
+
+
+// Методы жизненного цикла
+  // MOUNTTING - монтирование и оформление компонента в первый раз 
+    // constructor() => render => componentDitMount()
+      // componentDitMount - компонент уже подключен уже на странице
+
+  // UBDATES - обновление компонента
+    // new props - новые свойства
+                                  // => componentDitUpdate() - компонент обновился
+    // setState()
+
+  // UNMOUNTTING - удаление компонента
+    // componentWillUnmount() - компонент будет удален
+
+  // ERROR - ошибка
+    // componentDitCatch()
+
+
+
+
 
 
   render() {
-    const cards = this.state.cards;
+    console.log('render отобразился')
+    const data = this.state.data;
     console.log(this.state);
     return (
       <div>
-                <p>Вы нажали {this.state.count} раз</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          Нажми на меня
-        </button>
         <Nav />
         <Main>
           <LeftPanel/>
           <div className="content">
             <div className="orderMain">
-          {/* создание */}
-              {cards.map((elem, index)=>{
+              {/* создание */}
+              {data.map((elem, index)=>{
                 return(
-                  <Card time = {elem.time}
-                  createdDate = {elem.createdDate}
-                  createdTime = {elem.createdTime}
-                  status = {elem.status}
-                  quantityPerson = {elem.quantityPerson}
-                  quantityTime = {elem.quantityTime}
-                  name = {elem.name}
-                  phone = {elem.phone}
-                  hall = {elem.hall}
-                  onClick = {() => this.funcObgectDelete(index)}
-                  edit = {() => this.edit(elem,index)}/>
+                  <Card
+                    waiting= {elem.waiting}
+                    status= {elem.status}
+                    count= {elem.count}
+                    time= {elem.time}
+                    name={elem.name} 
+                    phone= {elem.phone}
+                    halls= {elem.halls}
+                    created= {elem.created}
+                    deleteComp = {() => this.funcObgectDelete(index)}
+                    edit = {() => this.edit(elem,index)}/>
+                  )
+                }
                 )
-              })}
-
+                }
             </div>
             <div className = "bottomPanel">
-            {/* <button className= 'buttonCreate' onClick = {this.funcObgect}>Добавить</button> */}
             <button className= 'buttonCreate' onClick = {this.openModal}>Добавить</button>
               <div className="bottomStatus">
                 Общее количество резервов 2(2). Всего гостей: 18(18)
@@ -242,52 +244,56 @@ class App extends Component {
           onRequestClose={this.closeModal}
           style={this.customStyles}
           contentLabel="Example Modal">
-
-<div className ='modalMain'>
-  <div className = 'change'>
-    <div>
+          <div className ='modalMain'>
+          <div className = 'change'>
+              <form>
                 <p>
-                    <label>Время:</label><br />
+                    <label>Время</label><br />
+                    <input type="text" value={this.state.waiting} onChange={this.handleChangeWaiting}/>
+                </p>
+                <p>
+                    <label>Статус</label><br />
+                    <input type="text" value={this.state.status} onChange={this.handleChangeStatus}/>
+                </p>
+              </form>
+              <form>
+                <p>
+                    <label>Количество:</label><br />
+                    <input type="text" value={this.state.count} onChange={this.handleChangeCount}/>
+                </p>
+                <p>
+                    <label>Окончание:</label><br />
                     <input type="text" value={this.state.time} onChange={this.handleChangeTime}/>
                 </p>
+              </form>
+              <form>
                 <p>
                     <label>Имя:</label><br />
                     <input type="text" value={this.state.name} onChange={this.handleChangeName}/>
                 </p>
-                </div>
-                <div>
                 <p>
                     <label>Телефон:</label><br />
                     <input type="text" value={this.state.phone} onChange={this.handleChangePhone}/>
                 </p>
-                <p>
-                    <label>Статус:</label><br />
-                    <input type="text" value={this.state.status} onChange={this.handleChangeStatus}/>
-                </p>
-                </div>
-                <div>
-                <p>
-                    <label>Заказчики:</label><br />
-                    <input type="text" value={this.state.quantityPerson} onChange={this.handleChangeQuantityPerson}/>
-                </p>
-                <p>
-                    <label>Обработка:</label><br />
-                    <input type="text" value={this.state.quantityTime} onChange={this.handleChangeQuantityTime}/>
-                </p>
-                </div>
-                </div>
-                <p>
-                    <label>Зал:</label><br />
-                    <input className ='hall' type="text" value={this.state.hall} onChange={this.handleChangeHall}/>
-                    <select>
-                      <option>Основной зал</option>
-                      <option>Малый зал</option>
-                  </select>
-                </p>
-                <button className = 'buttonSave' onClick = {this.handleSubmit}>Сохранить и выйти</button>
-                <img src = {require("./image/Delete-80_icon-icons.com_57340 (1).png")} 
-                className = 'buttonOff' onClick = {this.closeModal}/>
-                </div>
+              </form>
+            </div>
+            <form>
+            <p>
+              <label>Зал:</label><br />
+              <input className ='hall' type="text" value={this.state.halls} onChange={this.handleChangeHalls}/>
+                <select>
+                  <option>Основной зал</option>
+                  <option>Малый зал</option>
+              </select>
+            </p>
+            <p>
+              <label>Время создания:</label><br />
+              <input className = 'created' type="text" value={this.state.created} onChange={this.handleChangeCreated}/>
+            </p>
+            <button className = 'buttonSave' onClick = {this.handleSubmit}>Сохранить и выйти</button>
+            <img src = {require("./image/Delete-80_icon-icons.com_57340 (1).png")} className = 'buttonOff' onClick = {this.closeModal}/>
+            </form>
+          </div>
         </Modal>
       </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,56 +1,53 @@
 import React from 'react'
 
-const Card = (props) => {
+const Card = ({waiting,status,count,time,name,phone,halls,created, edit, deleteComp}) => {
     return (
         <div className="order">
         <div className="part" style = {{width: '70%'}}>
             <div className="position">
-                <div className="time">{props.time}</div>
+                <div className="time">{waiting}</div>
             </div>
             <div className="position">
-                <div className="expectation">{props.status}</div>
+                <div className="expectation">{status}</div>
             </div>
         </div>
         <div className="part" style = {{width: '70%'}}>
             <div className="position" style = {{width: '70%', display: 'flex', justifyContent: 'space-between'}}>
                 <img src={require("../image/group.png")}/>
-                <div>{props.quantityPerson}</div>
+                <div>{count}</div>
             </div>
             <div className="position" style = {{width: '70%', display: 'flex', justifyContent: 'space-between'}}>
                 <img src={require("../image/schedule.png")} style = {{paddingLeft: '7px'}}/>
-                <div>{props.quantityTime}</div>
+                <div>{time}</div>
             </div>
         </div>
         <div className="part" style = {{width: '170%'}}>
             <div className="partNumber">
                 <div className="position">
                     <img src={require("../image/account_box.png")}/>
-                    <div>{props.name}</div>
+                    <div>{name}</div>
                 </div>
                 <div className="position">
                     <img src={require("../image/local_phone.png")}/>
-                    <div>{props.phone}</div>
+                    <div>{phone}</div>
                 </div>
             </div>
         </div>
         <div className="part" style = {{width: '170%'}}>
             <div className="position">
                 <img src={require("../image/Union.png")}/>
-                <div>№{props.hall}.</div>
+                <div>Оснорвной {halls}.</div>
             </div>
         </div>
         <div className="part">
             <div className="position">
-                <div>{props.createdDate}</div>
-            </div>
-            <div className="position">
-                <div>{props.createdTime}</div>
+                <div>{created}</div>
             </div>
         </div>
         <div className="part" style = {{width: '50%', border: 'none', position: 'relative'}}>
             <div className="position">
-                <img onClick = {props.edit} src={require("../image/border_color.png")}/>
-                <img className = "delete" onClick = {props.onClick} src={require("../image/Delete-80_icon-icons.com_57340 (1).png")}/>
+                <img onClick = {edit} src={require("../image/border_color.png")}/>
+                <img className = "delete" onClick = {deleteComp} src={require("../image/Delete-80_icon-icons.com_57340 (1).png")}/>
             </div>
         </div>
     </div>
